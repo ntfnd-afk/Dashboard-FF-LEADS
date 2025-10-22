641dd976053ae78509657bea4a18d432442bd95c
ntfnd-afk <ntfndv@gmail.com>
2025-10-22 21:05:46 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ var globalTelegramSettings = {
 };
 
 // API Configuration
-var API_BASE_URL = 'https://api.fulfilment-one.ru/api'; // Основной API сервер
+var API_BASE_URL = 'http://51.250.97.39:3001/api'; // Основной API сервер
 var isOnline = navigator.onLine; // Реальный статус подключения
 
 // Проверка подключения
