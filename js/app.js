4ed41e6ee43989f9d808bddac9a428cae399f2e3
ntfnd-afk <ntfndv@gmail.com>
2025-10-22 21:59:16 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ var globalTelegramSettings = {
 };
 
 // API Configuration
-var API_BASE_URL = 'http://51.250.97.39:3001/api'; // Основной API сервер
+var API_BASE_URL = 'http://51.250.97.39/api'; // Через nginx прокси
 var isOnline = navigator.onLine; // Реальный статус подключения
 
 // Проверка подключения
